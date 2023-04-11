0329337f6c7bbaaef954da304930cade86b6d350
Abhi2359 <abhishekchoudhary1018@gmail.com>
2023-04-11 23:31:50 +0530

    admin authentication

--- backend/middleware/auth.js
+++ backend/middleware/auth.js
@@ -1,18 +1,29 @@
-const ErrorHandler = require('../utils/errorHandler');
-const catchAsyncErrors = require('./catchAsyncErrors')
-const jwt = require('jsonwebtoken');
+const ErrorHandler = require("../utils/errorHandler");
+const catchAsyncErrors = require("./catchAsyncErrors");
+const jwt = require("jsonwebtoken");
 const User = require("../models/userModel");
-exports.isAuthenticatedUser = catchAsyncErrors(async(req,res,next)=>{
-    
-    const {token} = req.cookies;
-    // console.log(token);
-    if(!token){
-        return next(new ErrorHandler("Please login  to access this resource"));
+exports.isAuthenticatedUser = catchAsyncErrors(async (req, res, next) => {
+  const { token } = req.cookies;
+  // console.log(token);
+  if (!token) {
+    return next(new ErrorHandler("Please login  to access this resource"));
+  }
+  const decodedData = jwt.verify(token, process.env.JWT_SECRET);
+  req.user = await User.findById(decodedData.id);
+
+  next();
+});
+
+exports.authorizeRoles = (...roles) => {
+   
+  return (req, res, next) => {
+    console.log(req.user.role);
+    if (!roles.includes(req.user.role)) {
+      return next (new ErrorHandler(
+        `Role: ${req.user.role} is not allowed to use this resource`,403
+      ));
+      
     }
-  const decodedData  = jwt.verify(token,process.env.JWT_SECRET );
-   req.user = await User.findById(decodedData.id);
-
-   next();
-
-
-})
+    next();
+  };
+};
